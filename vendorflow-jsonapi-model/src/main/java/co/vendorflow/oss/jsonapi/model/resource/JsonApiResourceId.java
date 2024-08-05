c92dc2123fe2d88bc96eb50f9a629e1b5590555f
Christopher Smith <chrylis@gmail.com>
2024-08-05 11:54:03 -0500

    Add collection builder for many IDs

--- vendorflow-jsonapi-model/src/main/java/co/vendorflow/oss/jsonapi/model/resource/JsonApiResourceId.java
+++ vendorflow-jsonapi-model/src/main/java/co/vendorflow/oss/jsonapi/model/resource/JsonApiResourceId.java
@@ -1,5 +1,10 @@
 package co.vendorflow.oss.jsonapi.model.resource;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.Collection;
+import java.util.List;
+
 import lombok.Value;
 
 @Value
@@ -20,5 +25,9 @@ public final class JsonApiResourceId implements HasJsonApiResourceId {
 
     public static JsonApiResourceId of(String type, Object id) {
         return new JsonApiResourceId(type, String.valueOf(id));
+    }
+
+    public static List<JsonApiResourceId> ofAll(String type, Collection<Object> ids) {
+        return ids.stream().map(id -> of(type, id)).collect(toList());
     }
 }
